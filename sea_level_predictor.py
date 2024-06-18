95602e901cd580b94ebb03bd5abc0cb74107d0ff
marcin krzesinski <m1.krzesinski@gmail.com>
2024-06-18 01:17:33 +0000

    Sea Level Predictor freeCodeCamp Project

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,19 +4,33 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
+    df = pd.read_csv('epa-sea-level.csv')
+    x=df['Year']
+    y=df['CSIRO Adjusted Sea Level']
 
     # Create scatter plot
-
+    plt.scatter(x, y)
 
     # Create first line of best fit
-
+    res = linregress(x, y)
+    x_pred = pd.Series([ i for i in range(1880, 2051) ])
+    y_pred = res.intercept + res.slope*x_pred
+    plt.plot(x_pred, y_pred, 'r')
 
     # Create second line of best fit
+    new_df = df[ df['Year'] >= 2000 ]
+    new_x=new_df['Year']
+    new_y=new_df['CSIRO Adjusted Sea Level']
 
+    res2 = linregress(new_x, new_y)
+    x_pred2 = pd.Series([ i for i in range(2000, 2051) ])
+    y_pred2 = res2.intercept + res2.slope*x_pred2
+    plt.plot(x_pred2, y_pred2, 'g')
 
     # Add labels and title
-
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
+    plt.title('Rise in Sea Level')
     
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
